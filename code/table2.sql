4ccba1a433be7b59d302fb2cf8f0203bc5f1e5d6
Tom Pollard <tpollard@mit.edu>
2015-12-07 15:44:09 -0500

    Template code for Table 2

---
+++ code/table2.sql
@@ -0,0 +1,8 @@
+-- ---------------------------------------------------------------------
+-- Title: Table 2: Details of the MIMIC-III population by care unit.
+-- MIMIC version: Written for MIMIC-III (v1.3)  
+-- Created by: Tom Pollard, Alistair Johnson
+-- References: Medical Information Mart for Intensive Care (MIMIC-III): 
+--			   the public access critical care database 
+-- ---------------------------------------------------------------------
+
