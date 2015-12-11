08e77269bdbd283265ed4f2e7fe5f7c75704e40e
Tom Pollard <tpollard@mit.edu>
2015-12-11 15:12:11 -0500

    add gitignore

--- code/table3.sql
+++
@@ -1,8 +0,0 @@
--- ---------------------------------------------------------------------
--- Title: Table 2: Distribution of ICD-9 codes by care unit.
--- MIMIC version: Written for MIMIC-III (v1.3)  
--- Created by: Tom Pollard, Alistair Johnson
--- References: Medical Information Mart for Intensive Care (MIMIC-III): 
---			   the public access critical care database 
--- ---------------------------------------------------------------------
-
